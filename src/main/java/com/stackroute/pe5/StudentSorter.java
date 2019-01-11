6eddbd1ea532184b43288ba1f7e6d9e99f92c3c4
aishwarya_g <aishu047@gmail.com>
2019-01-11 16:46:01 +0530

    modified StudentSorter files

--- src/main/java/com/stackroute/pe5/StudentSorter.java
+++ src/main/java/com/stackroute/pe5/StudentSorter.java
@@ -9,11 +9,11 @@ public class StudentSorter implements Comparator<Student>{
      */
     @Override
     public int compare(Student student1, Student student2) {
-        int flag;
-        if (student1.getAge() > student2.getAge()) {
-            flag = 1;
-        } else if (student1.getAge() < student2.getAge()) {
+        int flag = 0;
+        if (student1.getAge() < student2.getAge())   {
             flag = 0;
+        } else if (student1.getAge() > student2.getAge()){
+            flag = 1;
         } else {
             if (student1.getName().compareTo(student2.getName()) > 1) {//compareTo return 0 if equals postive if 1st string is greater and negative if 1st string is lesser
                 flag = 0;
@@ -21,10 +21,10 @@ public class StudentSorter implements Comparator<Student>{
                 flag = 1;
             }
             else {
-                if (student1.getId() > student2.getId()) {
-                    flag = 1;
-                } else
+                if (student1.getId() < student2.getId()) {
                     flag = 0;
+                } else
+                    flag = 1;
             }
         }
         return flag;
